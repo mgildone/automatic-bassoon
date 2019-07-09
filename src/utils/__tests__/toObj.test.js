5ae6c417779fa5b8413296a41870dbc1aecddfbd
Matteo Gildone <44809484+mgildone@users.noreply.github.com>
2019-07-09 15:18:30 +0100

    test

--- src/utils/__tests__/toObj.test.js
+++ src/utils/__tests__/toObj.test.js
@@ -1,10 +1,23 @@
-import { toObj } from "../toObj";
+import { toObj, toMergedObj } from "../toObj";
 
 describe("toObj", () => {
   it("Should return an object", () => {
     const mockFn = jest.fn(x => x);
     const expected = { key: "value" };
     const actual = toObj(mockFn)("key")("value");
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("toMergedObj", () => {
+  it("Should merge a passed object and return a new object", () => {
+    const mockFn = jest.fn(x => "value");
+    const mockFnToObj = toObj(mockFn)("key2");
+    const mockFnToMergeObj = toMergedObj(mockFnToObj);
+    const expected = { key: "value", key2: "value" };
+    const actual = mockFnToMergeObj({
+      key: "value"
+    });
     expect(actual).toEqual(expected);
   });
 });
